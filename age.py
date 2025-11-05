7cf478cec923da60ca22ab5e777a7e659271ca08
PrakashGrg <rabig6885@gmail.com>
2025-11-05 20:22:45 +0545

    age and marks

---
+++ age.py
@@ -0,0 +1,7 @@
+age = 20
+if age < 18:
+    print("Minor")
+elif age == 18:
+    print("Just became an adult")
+else:
+    print("Adult")
---
+++ marks.py
@@ -0,0 +1,12 @@
+marks = 73
+
+if marks >= 90:
+    print("Distinction")
+elif marks >= 80:
+    print("First Division")
+elif marks >= 70:
+    print("Pass with Merit")
+elif marks >= 45:
+    print("pass")
+else:
+    print("Fail")
