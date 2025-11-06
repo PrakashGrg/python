8ddf0c5d9703b11275d214421ca8999ffbca099a
PrakashGrg <rabig6885@gmail.com>
2025-11-06 11:20:55 +0545

    Type and age

---
+++ Type.py
@@ -0,0 +1,11 @@
+age = 23
+old = False
+height = 5.9
+a = None
+name = "Alice"
+
+print(type(name))
+print(type(age))
+print(type(old))
+print(type(height))
+print(type(a))
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+name ="Jester"
+age =22
+date ="2024-06-15"
+
+print(type(name))
+print(type(age))
+print(type(date))
